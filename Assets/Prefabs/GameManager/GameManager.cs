1dfb80029316ba75c3e8287d607198485d193da7
Sadeiko <Xovval@users.noreply.github.com>
2023-12-19 02:52:17 +0100

    Add assets (partially)

--- Assets/Prefabs/GameManager/GameManager.cs
+++ Assets/Prefabs/GameManager/GameManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    
     private static GameManager _instance;
 
     public static GameManager Instance { get { return _instance; } }
